b2a26f8cf1fbd49649fb678f16201a5719b793e6
Marbelous <maksimikk@gmail.com>
2018-02-08 14:26:19 -0500

    Added mail and messaging events

---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    public class VideoEncoder
+    {
+        // 1- Define a delegate
+        // 2- Define an event based on that delegate
+        // 3- Raise the event
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args); // 1
+
+        public event VideoEncodedEventHandler VideoEncoded; // 2
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(); // 3
+        }
+
+        protected virtual void OnVideoEncoded() // Standard C# event signature
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, EventArgs.Empty);
+        }
+    }
+}
